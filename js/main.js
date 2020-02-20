e5f4391cc40839fe7881d4bbbb3dde68c371dc9f
aliahmadcse <aliahmadcse@gmail.com>
2020-02-20 21:02:35 +0500

    Adds js script for owl carousel

--- js/main.js
+++ js/main.js
@@ -20,4 +20,20 @@ $(document).ready(function() {
     type: "image",
     gallery: { enabled: true }
   });
+
+  // Owl-carousel
+
+  $(".site-main .about-area .owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      560: {
+        items: 2
+      }
+    }
+  });
 });
